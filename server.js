11635f46389cfc1d63762e31c128393cbb734b3e
rtuttleGit <Ryan@ryans-mbp.frontier.com>
2015-10-03 18:29:56 -0400

    changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var participants = [];
 /* Server config */
 
 //Server's IP address
-app.set("ipaddr", "127.0.0.1");
+//app.set("ipaddr", "127.0.0.1");
 
 //Server's port number
 app.set("port", 1337);
